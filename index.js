2ee760e2505ae1e4b435dc2015b3f831e924eb62
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-04 18:52:44 +0300

    commit tree e4a494d39bc56bc0553c28030cde3e24c2bd550c

---
+++ index.js
@@ -0,0 +1,69 @@
+//add function
+function add() {
+    
+}
+add();
+ 
+//subtract function
+function subtract(a, b) {
+   return 10 - 5;
+}
+subtract(10, 5);
+
+//multiply function
+function multiply(a, b) {
+    return a * b;
+}
+multiply(10, 5);
+
+//divide function
+function divide(a, b) {
+    return a / b;
+}
+divide(a, b);
+
+//add function
+function add(a, b) {
+    return a + b;
+}
+add(888,600);
+
+//subtract function
+function subtract(a, b) {
+    return a - b;
+}
+subtract(5, 137);
+
+//increment operator
+function increment(n) {
+    return (n += 1);
+}
+increment(10);
+
+//decrement operator
+function decrement(n) {
+    return (n -= 1);
+}
+decrement(10);
+
+//assume a base of 10
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+makeInt("10");
+
+//parseFloat
+function preserveDecimal(n){
+    return n = 2.222;
+}
+parseFloat('2.222');
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+preserveDecimal("sldkjflksjf");
+
+
+
+
+
